ae668e0005951ecac73c3b7db921ff5110d0cba3
edgargmc <edgargmc@gmail.com>
2017-04-08 17:35:56 -0300

    Reposition the cursor AND Clear the screen on exit

--- kilo.c
+++ kilo.c
@@ -5,51 +5,83 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/*** data ***/
+
 struct termios orig_termios;
 
-void die(const char *s){
-	perror(s);
-	exit(1);
+/*** terminal ***/
+
+void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  perror(s);
+  exit(1);
 }
 
-void disableRawMode(){
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
-	  die("tcsetattr");    
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
 }
 
 void enableRawMode() {
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1 ) die("tcgetattr");
-	atexit(disableRawMode);
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  atexit(disableRawMode);
 
-	struct termios raw = orig_termios;
-	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-	raw.c_oflag &= ~(OPOST);
-	raw.c_cflag |= (CS8);
-	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG );
-	raw.c_cc[VMIN] = 0;
-	raw.c_cc[VTIME] = 1;
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
 
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-int main(){
-	enableRawMode();
-
-	while(1){
-	char c = '\0';
-	if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-		if(iscntrl(c)){
-			printf("%d\r\n", c);	
-		}else{
-			printf("%d ('%c')\r\n", c, c);
-		}
-	if(c == 'q') break;
-	}
-	return 0;
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
 }
 
+/*** output ***/
 
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
 
+/*** input ***/
 
+void editorProcessKeypress() {
+  char c = editorReadKey();
 
+  switch (c) {
+    case CTRL_KEY('q'):
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+  	write(STDOUT_FILENO, "\x1b[H", 3);
+	exit(0);
+        break;
+  }
+}
 
+/*** init ***/
+
+int main() {
+  enableRawMode();
+
+  while (1) {
+    editorRefreshScreen();
+    editorProcessKeypress();
+  }
+
+  return 0;
+}
